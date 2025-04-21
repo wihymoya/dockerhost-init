7d2d7bb6cefaea4c30487f7c35eb7176ad9b7226
wihymoya <wyang87+github@gmail.com>
2025-04-21 19:48:50 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,82 @@
 #!/bash/bin
+# -----------------------------------------------------------------------------------------------------------------------------
+# REMEMBER TO DOT RUN THIS SCRIPT
+# user@host:~$ . ./gcm.sh
+# -----------------------------------------------------------------------------------------------------------------------------
+
+
+# Git Credential Installer
+# -----------------------------------------------------------------------------------------------------------------------------
+# Configure $GPG_PASSPHRASE via active prompt
+export GPG_PASSPHRASE=$(read -sp 'GPG_PASSPHRASE: ' gpg_pw; echo $gpg_pw); echo ''
+
+# Update package lists
+sudo apt-get update
+
+# Install required packages
+sudo apt-get --yes install gpg pass 
+
+# Export GCM vars
+export GCM_CREDENTIAL_STORE=gpg
+export GPG_TTY=$(tty)
+
+# Run gpg to generate key pair and UID of 'USER_ID'. You must set $GPG_PASSPHRASE before running this script
+gpg --batch --passphrase $GPG_PASSPHRASE --quick-gen-key USER_ID default default never
+
+# Unset $GPG_PASSPHRASE as it is no longer necessary
+unset GPG_PASSPHRASE
+
+# Initialize credential store
+pass init USER_ID
+
+# Download latest GCM deb package
+wget "https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.6.1/gcm-linux_amd64.2.6.1.deb"
+
+# Install GCM package
+sudo dpkg -i gcm-linux_amd64.2.6.1.deb
+
+# Remove GCM deb package
+sudo rm gcm-linux_amd64.2.6.1.deb
+
+# Configure GCM
+git-credential-manager configure
+# -----------------------------------------------------------------------------------------------------------------------------
+
+
+# Install Docker
+# -----------------------------------------------------------------------------------------------------------------------------
+# Remove old and conflicting packages
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Update package lists
+sudo apt-get update
+
+# Install required packages
+sudo apt-get --yes install ca-certificates curl
+
+# Create required directory and modify permissions
+sudo install -m 0755 -d /etc/apt/keyrings
+
+# Add Docker's official GPG key
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+
+# Modify permissions of the GPG key
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Set up the stable repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update package lists again to include Docker's repository
+sudo apt-get update
+
+# Install docker engine
+sudo apt-get --yes install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add current user to the docker group for sudo-less docker commands
+sudo usermod -aG docker $USER
+
+# Activate changes to groups
+newgrp docker
+# -----------------------------------------------------------------------------------------------------------------------------
