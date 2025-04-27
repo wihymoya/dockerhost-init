91c41ed3d23a2d32bda236c2a7ae7a36bae36138
wihymoya <wyang87+github@gmail.com>
2025-04-27 14:33:55 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,6 +5,19 @@
 # user@host:~$ source ./startup.sh
 
 # --------------------------------
+# Disable port 53 on host
+
+# Disable stub resolver
+sudo sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
+
+# Update symlink to point to /run/systemd/resolve/resolv.conf, which is automatically updated to follow the system's netplan
+sudo sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
+
+# Restart systemd-resolved service
+sudo systemctl restart systemd-resolved
+# --------------------------------
+
+# --------------------------------
 # Git Credential Manager
 
 # Configure $gpg_passphrase via active prompt
@@ -103,15 +116,3 @@ sudo usermod -aG docker $USER
 newgrp docker
 # --------------------------------
 
-# --------------------------------
-# Disable port 53 on host
-
-# Disable stub resolver
-sudo sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
-
-# Update symlink to point to /run/systemd/resolve/resolv.conf, which is automatically updated to follow the system's netplan
-sudo sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
-
-# Restart systemd-resolved service
-sudo systemctl restart systemd-resolved
-# --------------------------------
